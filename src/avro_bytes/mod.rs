bd9452a33eeefeb7142d3e52d49ffd49169dd01b
Yannick Guern <dev@guern.eu>
2024-03-27 14:32:36 +0100

    clean imports

--- src/avro_bytes/mod.rs
+++ src/avro_bytes/mod.rs
@@ -2,65 +2,66 @@ mod de;
 mod ser;
 
 pub mod bytes {
-    use crate::avro_bytes::de;
-    use crate::avro_bytes::ser;
+    pub use super::*;
+
     #[allow(unused)]
     pub use de::bytes::deserialize_bytes as deserialize;
     #[allow(unused)]
     pub use ser::bytes::serialize_bytes as serialize;
 
     pub mod option {
+        pub use super::*;
         #[allow(unused)]
-        pub use crate::avro_bytes::de::bytes::deserialize_option_bytes as deserialize;
+        pub use de::bytes::deserialize_option_bytes as deserialize;
         #[allow(unused)]
-        pub use crate::avro_bytes::ser::bytes::serialize_option_bytes as serialize;
+        pub use ser::bytes::serialize_option_bytes as serialize;
     }
 }
 
 pub mod hashmap {
+    pub use super::*;
     #[allow(unused)]
     pub use de::map::deserialize_hashmap as deserialize;
-
-    use crate::avro_bytes::de;
     #[allow(unused)]
-    pub use crate::avro_bytes::ser::map::serialize_hashmap as serialize;
+    pub use ser::map::serialize_hashmap as serialize;
 
     pub mod option {
+        pub use super::*;
         #[allow(unused)]
-        pub use crate::avro_bytes::de::map::deserialize_option_hashmap as deserialize;
+        pub use de::map::deserialize_option_hashmap as deserialize;
         #[allow(unused)]
-        pub use crate::avro_bytes::ser::map::serialize_option_hashmap as serialize;
+        pub use ser::map::serialize_option_hashmap as serialize;
     }
 }
 
 pub mod btreemap {
+    pub use super::*;
     #[allow(unused)]
     pub use de::map::deserialize_btreemap as deserialize;
-
-    use crate::avro_bytes::de;
     #[allow(unused)]
-    pub use crate::avro_bytes::ser::map::serialize_btreemap as serialize;
+    pub use ser::map::serialize_btreemap as serialize;
 
     pub mod option {
+        pub use super::*;
         #[allow(unused)]
-        pub use crate::avro_bytes::de::map::deserialize_option_btreemap as deserialize;
+        pub use de::map::deserialize_option_btreemap as deserialize;
         #[allow(unused)]
-        pub use crate::avro_bytes::ser::map::serialize_option_btreemap as serialize;
+        pub use ser::map::serialize_option_btreemap as serialize;
     }
 }
 
 pub mod list {
+    pub use super::*;
     #[allow(unused)]
     pub use de::list::deserialize_list as deserialize;
-
-    use crate::avro_bytes::de;
     #[allow(unused)]
-    pub use crate::avro_bytes::ser::list::serialize_list_bytes as serialize;
+    pub use ser::list::serialize_list_bytes as serialize;
 
     pub mod option {
+        pub use super::*;
         #[allow(unused)]
-        pub use crate::avro_bytes::de::list::deserialize_option_list as deserialize;
+        pub use de::list::deserialize_option_list as deserialize;
         #[allow(unused)]
-        pub use crate::avro_bytes::ser::list::serialize_option_list_bytes as serialize;
+        pub use ser::list::serialize_option_list_bytes as serialize;
     }
 }
